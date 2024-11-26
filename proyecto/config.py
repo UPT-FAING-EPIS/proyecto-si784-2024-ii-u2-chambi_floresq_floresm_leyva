1fe56f265a4e800154efda1ecf12e4883c57a2bd
jf2021070309 <jaimeelias.tacna.2016@gmail.com>
2024-11-26 00:58:36 -0500

    Version 09 - Implementacion de sonnarcloud

--- proyecto/config.py
+++ proyecto/config.py
@@ -1,5 +1,8 @@
-server = 'DESKTOP-UQQRNID'
-database = 'CasaDeCambioDB'
-username = 'sa'
-password = 'sa'
+import os
+
+# Obtener las variables de entorno, si no están definidas, usar valores predeterminados
+server = os.getenv('DB_SERVER', '.')  # Por defecto usa '.' (localhost)
+database = os.getenv('DB_NAME', 'CasaDeCambioDBTest')  # Usar 'CasaDeCambioDB' por defecto
+username = os.getenv('DB_USERNAME', 'sa')
+password = os.getenv('DB_PASSWORD', 'epis')
 driver = '{ODBC Driver 17 for SQL Server}'
